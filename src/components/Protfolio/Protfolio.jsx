267909f0a60827fee189d2607e1fe51ca9049477
Yameen <yameenmuhammad960@gmail.com>
2023-10-26 02:56:34 +0500

    added new portfolio

--- src/components/Protfolio/Protfolio.jsx
+++ src/components/Protfolio/Protfolio.jsx
@@ -4,19 +4,26 @@ import IMG1 from "../../assets/project1.jpg";
 import IMG2 from "../../assets/project22.jpg";
 import IMG3 from "../../assets/portfolio33.jpg";
 import IMG4 from "../../assets/portfolio44.jpg";
-// import IMG5 from "../../assets/portfolio5.png";
+import IMG5 from "../../assets/portfolio55_light.jpg";
 // import IMG6 from "../../assets/portfolio6.jpg";
 
 const DATA = [
   {
     id: 1,
-    image: IMG1,
-    title: "Fitness Website: Built in React.js and TypeScript.",
-    github: "https://github.com/myameen123/gym-website",
-    demo: "https://gym-website-yameen.netlify.app/",
+    image: IMG5,
+    title: "Notion Clone: using advance concepts of Next.js",
+    github: "https://github.com/myameen123/notion-clone",
+    demo: "https://myameennotes.vercel.app/",
   },
   {
     id: 2,
+    image: IMG3,
+    title: "MS TODO CLONE: Built in React.js, with Redux for State Management.",
+    github: "https://github.com/myameen123/ms-todo-clone",
+    demo: "https://ms-todo-clone-yameen.vercel.app/",
+  },
+  {
+    id: 3,
     image: IMG2,
     title:
       "I have built this website for a radwi company. using Next.js and Express.js.",
@@ -24,27 +31,21 @@ const DATA = [
     demo: "https://www.rad-wi.com/",
   },
   {
-    id: 3,
-    image: IMG3,
-    title: "MS TODO CLONE: Built in React.js, with Redux for State Management.",
-    github: "https://github.com/myameen123/ms-todo-clone",
-    demo: "https://ms-todo-clone-yameen.vercel.app/",
-  },
-  {
     id: 4,
     image: IMG4,
     title:
-      "Mdx BLog: A blog app using MDX and Gatsby. The app covers all the core concepts of working with mdx plugin in a gatsby project.",
+      "Mdx BLog: A blog app using MDX and Gatsby, with mdx plugin in a gatsby project.",
     github: "https://github.com/myameen123/MDX-Blog",
     demo: "https://mdx-blog-posts-gatsby.netlify.app/",
   },
-  // {
-  //   id: 5,
-  //   image: IMG5,
-  //   title: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-  //   github: "https://github.com",
-  //   demo: "hhtps://livedemo.com",
-  // },
+  {
+    id: 5,
+    image: IMG1,
+    title: "Fitness Website: Built in React.js and TypeScript.",
+    github: "https://github.com/myameen123/gym-website",
+    demo: "https://gym-website-yameen.netlify.app/",
+  },
+
   // {
   //   id: 6,
   //   image: IMG6,
