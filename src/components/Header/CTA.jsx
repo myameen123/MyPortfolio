c22325f3f7b72e12eb8df62bf809ac8a9083f550
Muhammad Yameen <yameenmuhammad960@gmail.com>
2024-02-29 18:07:55 +0500

    update resume

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/Muhammad_Yameen_CV.pdf";
 export default function CTA() {
   return (
     <div className="cta">
